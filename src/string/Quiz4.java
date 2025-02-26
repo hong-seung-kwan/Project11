3584a048e0dcea533876814295069a03e888bb4f
hongseungkwan <hh8164@naver.com>
2025-02-26 17:50:31 +0900

    수정

--- src/string/Quiz4.java
+++ src/string/Quiz4.java
@@ -11,9 +11,10 @@ public class Quiz4 {
 		System.out.println(result2);
 		getCharCount3("자바 프로그래밍 입문");
 		getCharCount3("equals 메소드는 두 객체가 같은지 비교한다");
+		getCharCount4("자바 프로그래밍 입문");
 	}
 
-	// 1.
+	// 1. 리턴값 o
 	public static int getCharCount(String str) {
 		int strLength = 0;
 
@@ -24,14 +25,24 @@ public class Quiz4 {
 		}
 		return strLength;		
 	}
-	// 2.
+	// 2. 리턴값 o
 	public static int getCharCount2(String str) {
 		String newStr = str.replace(" ", "");
 		return newStr.length();
 	}
-	// 3.
+	// 3. 리턴값 x
 	public static void getCharCount3(String str) {
 		String newStr = str.replace(" ", "");
 		System.out.println(newStr.length());
+	}
+	public static void getCharCount4(String str) {
+		int strLength = 0;
+	// 4. 리턴값 x
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != ' ') {
+				strLength++;
+			}
+		}
+		System.out.println(strLength);		
 	}
 }
